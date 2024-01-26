8b96da31e236e1743660ad7233b97425638a1d25
nariama <155675556+nariama@users.noreply.github.com>
2024-01-26 10:46:28 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let celsius = parseInt(prompt("Ingrese temperatura en °Celsius"));
+
+let convertir = celsius * 1.8 + 32
+document.write(convertir + "°Fahrenheit")
+
+let convertir2 = celsius + 273.15
+document.write(convertir2 + "°Kelvin")
